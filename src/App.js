f81e7b091854447ad93283c4e1ca675429cec8cf
sunpinenuts <904975251@qq.com>
2023-04-17 15:50:12 +0800

    项目基础配置

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './pages/Layout'
+// import Login from './pages/Login'
+import Login from '@/pages/Login'
+import { Button } from 'antd'
+
 function App () {
   return (
     <div className="App">
-      app
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Button type="primary">Button</Button>
     </div>
   )
 }
